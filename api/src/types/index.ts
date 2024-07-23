953bd6e846ac5140d26dabf99a37cea2af4d6c27
aadarshprajapati777 <aadarsh9811594152@gmail.com>
2024-07-23 19:26:47 +0530

    export a custom data type from the file

--- api/src/types/index.ts
+++ api/src/types/index.ts
@@ -1,6 +1,6 @@
 export const CREATE_ORDER = "CREATE_ORDER";
 export const CANCEL_ORDER = "CANCEL_ORDER";
-
+export const GET_OPEN_ORDER = "GET_OPEN_ORDER";
 
 export type ResponseFromOrderbook = {
     type: "DEPTH",
