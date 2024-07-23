3fb663c34ec60ece2ce14d89273b806597602953
aadarshprajapati777 <aadarsh9811594152@gmail.com>
2024-07-23 17:24:08 +0530

    add: custom data types for Response from the order book

--- api/src/types/index.ts
+++ api/src/types/index.ts
@@ -1,2 +1,42 @@
 export const CREATE_ORDER = "CREATE_ORDER";
 export const CANCEL_ORDER = "CANCEL_ORDER";
+
+
+export type ResponseFromOrderbook = {
+    type: "DEPTH",
+    payload: {
+        market: string,
+        bids: [string, string][],
+        asks: [string, string][],
+    }
+} | {
+    type: "ORDER_PLACED",
+    payload: {
+        orderId: string,
+        executedQty: number,
+        fills: [
+            {
+                price: string,
+                qty: number,
+                tradeId: number
+            }
+        ]
+    }
+} | {
+    type: "ORDER_CANCELLED",
+    payload: {
+        orderId: string,
+        executedQty: number,
+        remainingQty: number
+    }
+} | {
+    type: "OPEN_ORDERS",
+    payload: {
+        orderId: string,
+        executedQty: number,
+        price: string,
+        quantity: string,
+        side: "buy" | "sell",
+        userId: string
+    }[]
+}
