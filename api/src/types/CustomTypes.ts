fec8d2c19651401ebad63da88658736a358ae99c
aadarshprajapati777 <aadarsh9811594152@gmail.com>
2024-07-23 19:25:48 +0530

    add: custom data type for get_open_order

--- api/src/types/CustomTypes.ts
+++ api/src/types/CustomTypes.ts
@@ -1,4 +1,4 @@
-import { CANCEL_ORDER, CREATE_ORDER } from ".";
+import { CANCEL_ORDER, CREATE_ORDER, GET_OPEN_ORDER } from ".";
 
 export type OrderData = {
 
@@ -17,6 +17,12 @@ export type OrderData = {
     type: typeof CANCEL_ORDER,
     data: {
         orderID: string,
+        market: string
+    }
+} | {
+    type: typeof GET_OPEN_ORDER,
+    data: {
+        userID: string,
         market: string
     }
 }
