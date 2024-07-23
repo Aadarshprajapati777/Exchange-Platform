e54bf3d53f832d5dcced2cd182b6f41450a4b021
aadarshprajapati777 <aadarsh9811594152@gmail.com>
2024-07-23 16:53:48 +0530

    send post order to RedisManager file for adding to queue and pubsub

--- api/src/routes/order.ts
+++ api/src/routes/order.ts
@@ -1,7 +1,23 @@
 import { Router } from "express";
+import { RedisManager } from "../RedisManager";
+import { CREATE_ORDER } from "../types";
 
 export const orderRouter = Router();
 
-orderRouter.get("/", (req,res) => {
-    res.send("this is order route")
+orderRouter.post("/", async (req,res) => {
+    const {market, price, side, quantity, userID} = req.body;
+    console.log({market, side, quantity, userID, price});
+
+    //send it to redis queue and subscribe to pubsub
+    const response = await RedisManager.getInstance().sendToQueueAndPubSub({
+        type: CREATE_ORDER,
+        data: {
+            market,
+            side,
+            quantity,
+            userID,
+            price
+        }
+    })
+    return res.json(response)
 })
