610fec81b3e86218524e9d57579e11bdde15b559
aadarshprajapati777 <aadarsh9811594152@gmail.com>
2024-07-23 19:24:43 +0530

    add: delete and open route

--- api/src/routes/order.ts
+++ api/src/routes/order.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { RedisManager } from "../RedisManager";
-import { CREATE_ORDER } from "../types";
+import { CANCEL_ORDER, CREATE_ORDER, GET_OPEN_ORDER } from "../types";
 
 export const orderRouter = Router();
 
@@ -20,4 +20,27 @@ orderRouter.post("/", async (req,res) => {
         }
     })
     return res.json(response.payload)
+})
+
+orderRouter.delete("/", async (req,res) => {
+    const {orderID, market} = req.body;
+    const response = await RedisManager.getInstance().sendToQueueAndPubSub({
+        type: CANCEL_ORDER,
+        data: {
+            orderID,
+            market
+        }
+    })
+    return res.json(response.payload);
+})
+
+orderRouter.get("/open", async (req,res) => {
+    const response = await RedisManager.getInstance().sendToQueueAndPubSub({
+        type: GET_OPEN_ORDER,
+        data: {
+            userID: req.query.userID as string,
+            market: req.query.market as string
+        }
+    })
+    return res.json(response.payload);
 })
