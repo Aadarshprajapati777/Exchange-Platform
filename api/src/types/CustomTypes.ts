4af67a884521f25107805394d993707566e0b742
aadarshprajapati777 <aadarsh9811594152@gmail.com>
2024-07-23 16:50:19 +0530

    add: custom data types

---
+++ api/src/types/CustomTypes.ts
@@ -0,0 +1,24 @@
+import { CANCEL_ORDER, CREATE_ORDER } from ".";
+
+export type OrderData = {
+
+    //order data can be of multiple types like cancel_order, post_order
+    //market, price, quantity, side, userId
+
+    type: typeof CREATE_ORDER,
+    data:{
+        market: string;
+        quantity: number;
+        side: "buy || sell";
+        price: number;
+        userID: string;
+    } | {
+        type: typeof CANCEL_ORDER,
+        data: {
+            keys: "values"
+        }
+    }
+    
+
+
+}
