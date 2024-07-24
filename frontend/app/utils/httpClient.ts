4b67bcedda1e552c9d41d9f49767d13b0a4253dc
aadarshprajapati777 <aadarsh9811594152@gmail.com>
2024-07-24 21:04:20 +0530

    local function filtering and returning ticker data for a single market

--- frontend/app/utils/httpClient.ts
+++ frontend/app/utils/httpClient.ts
@@ -3,6 +3,14 @@ import { Ticker } from "./types";
 
 const BASE_URL = "http://localhost:3000/api/v1";
 
+export async function getTicket(market: string): Promise<Ticker> {
+    const tickers = await getTickers();
+    const ticker = tickers.find(t => t.symbol === market);
+    if(!ticker){
+        throw new Error (`no ticker found for the market ${market}`);
+    }
+    return ticker;
+}
 
 export async function getTickers(): Promise<Ticker[]> {
     const response = await axios.get(`${BASE_URL}/ticker`);
